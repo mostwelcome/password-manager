c134e76cb2031e4374f5b8c03e0c5e9aea891ee7
krish1010 <krish.krishanu10@gmail.com>
2021-07-07 20:49:15 +0530

    refactored to string module instead of hadcoded lists

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import random
 import pyperclip
 import json
+import string
 
 # ---------------------------- Search Password ------------------------------- #
 
@@ -28,11 +29,8 @@ def generate_password():
     if len(password_entry.get()) !=0:
         password_entry.delete(0,END)
     # Password Generator 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
+    letters, numbers, symbols = string.ascii_letters, string.digits, string.punctuation
+    all_chars = f'{letters}{numbers}{symbols}'
     nr_letters = random.randint(8,10)
     nr_symbols = random.randint(2,4)
     nr_numbers = random.randint(2,4)
