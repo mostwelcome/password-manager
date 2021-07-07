8d1de56793a73a612f3c611ac42459899fb73387
krish1010 <krish.krishanu10@gmail.com>
2021-07-07 21:11:35 +0530

    added constants.py

---
+++ password-manager/constants.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+LETTERS, DIGITS, SYMBOLS = string.ascii_letters, string.digits, string.punctuation
+NO_OF_LETTERS = random.randint(8, 10)
+NO_OF_DIGITS = random.randint(2, 4)
+NO_OF_SYMBOLS = random.randint(2, 4)
